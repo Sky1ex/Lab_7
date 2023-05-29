5eba11f8d55e3b57a4355a84b5991f90e48df1a4
Vladislav Chivanov <vlad20041972@mail.ru>
2023-05-29 09:17:21 +0300

    Описание функций класса weather

---
+++ WeatherService/Weather.cpp
@@ -0,0 +1,18 @@
+#include "Weather.h"
+
+Weather::Weather(string city, double lon, double lat, double temperature, string weather, double windSpeed, int clouds) // Конструктор для присваивания знчений
+{
+	this->city = city;
+	this->lon = lon;
+	this->lat = lat;
+	this->temperature = temperature;
+	this->weather = weather;
+	this->windSpeed = windSpeed;
+	this->clouds = clouds;
+}
+
+void Weather::Print() //функция вывода
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Город: " << city << "\nДолгота и широта: " << lon << ", " << lat << "\nТемпература: " << temperature << "\nПогода: " << weather << "\nВетренность: " << windSpeed << "\nОблачность: " << clouds;
+}
